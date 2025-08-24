8019360c22fa3f1ca78391fa25447333f26440a0
ajju03 <ajayprasanna7@gmail.com>
2025-08-24 09:02:51 +0530

    File created to create syntax error

--- ajay.sh
+++ ajay.sh
@@ -4,11 +4,13 @@ echo "This is a file to create syntax error"
 
 echo "If you want to make a error type 'Y/y'"
 
-read (input)
+read input
 
 if [ "$input" == "Y" ] || [ "$input" == "y" ]; then
         echo "syntax error created"
-	echo" This is error.
+	echo "Forcing a syntax error..."
+        #Intentionally cause an error
+        some_nonexistent_command
 
 else
 	echo "Function ended."
